64213411824b7345e77db1d08b0a5577492a7e37
Kateryna Strough <katerynastrough@hotmail.com>
2019-08-04 02:55:59 +0000

    Automatically backed up by Learn

--- application.rb
+++ application.rb
@@ -2,8 +2,7 @@ class Application
 
   def call(env)
     resp = Rack::Response.new
-    resp.write "Hello, World"
-    resps.write "Hello, my name is Kateryna"
+    resp.write "Hello, my name is Kateryna"
     resp.finish
   end
 
